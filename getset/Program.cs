38d24d191f11cdad8b85bef4f8d8f0dc6d25a41f
dipak505 <dipakbapodra0111@gmail.com>
2023-08-07 17:10:43 +0530

    Add project files.

---
+++ getset/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student objstud = new Student()
+            {
+                Id = 1, Name = "Dipak",Marks = 90.99F,
+            };
+
+            Console.WriteLine(objstud.Id);
+            Console.WriteLine(objstud.Name);
+            Console.WriteLine(objstud.Marks);
+            Console.ReadKey();
+
+        }
+
+        public class Student
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public float Marks { get; set; }
+
+            private string Email { get; set; }
+        }
+    }
+}
